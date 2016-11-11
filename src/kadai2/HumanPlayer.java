220a419854afc00105ee138e55dff35bd0daa3f7
c16310 <c16310@pc2-01.suntech.ac.jp>
2016-11-11 12:32:24 +0900

    演習15_5　追加

--- src/kadai2/HumanPlayer.java
+++ src/kadai2/HumanPlayer.java
@@ -13,11 +13,11 @@ public class HumanPlayer extends Player {
     }
     
     //キーボードからジャンケンの手を読み込むようにする//
-    public   {
+    public int nextHand(){
         do {
             System.out.println("ジャンケン開始ジャンケンの手(0..グー/1..チョキ/2..パー)");
             hand = stdIn.nextInt();
         } while (hand < 0 || hand > 2);
-        return ;
+        return hand;
     }
 }
